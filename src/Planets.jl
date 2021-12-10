7a5e0c37f2eb73600125a069806f55b972256e6a
David James <davidabraham@ucla.edu>
2021-12-10 13:57:22 -0800

    adding export of Planet

--- src/Planets.jl
+++ src/Planets.jl
@@ -1,10 +1,15 @@
+# structure based off of Stixrude '21 et al.
+# design still being added to it
 module Planets
+
+export Planet
 
 struct Planet
 
     # structure parameters
     layers :: Real        # number of layers in structure file
     R      :: Real        # radius of planet
+    M      :: Real        # mass of planet
 
     # orbital parameters
     ω :: Real           # rotational frequency
@@ -17,12 +22,14 @@ struct Planet
     P0   :: Real        # phase transition reference P
     a    :: Real        # Simon pressure
     b    :: Real        # Simon gradient
+    B    :: Real        # effective temperature exponent
     ∇    :: Real        # adiabtatic gradient
     T_eq :: Real        # radiative equilibrium temperature
 
     # rheaology parameters
     η0         :: Real    # reference viscosity
     A          :: Real    # viscosity exponent
+    Ra         :: Real    # critical Rayleigh number
     γ          :: Real    # Andrade exponent
     μ_f        :: Real    # standard linear solid parameter
     rhea_model :: Real    # modulus model to use
