d00084635e07b6c0274d988791bad5aaf48e1efe
David James <davidabraham@ucla.edu>
2024-05-23 18:15:22 -0700

    added thermal_inertia

---
+++ src/Planets.jl
@@ -0,0 +1,184 @@
+module Planets
+
+include("structure.jl")
+include("thermal.jl")
+
+using PhysicalConstants.CODATA2014: σ
+
+export Planet, Moon
+export lumin_internal, lumin_core
+
+"""
+    Planet
+
+Planet struct used for PlanetEvolution.jl.
+
+# Fields
+- `name       :: String`                - name of planet
+- `layers     :: Int64`                 - number of layers in structure file
+- `R          :: Float64`               - radius of planet
+- `M          :: Float64`               - mass of planet
+- `GM         :: Float64`               - gravitational mass of planet
+- `ω          :: Float64`               - rotational frequency
+- `C_p        :: Union{Int64, Float64}` - specific heat
+- `α          :: Float64`               - thermal expansivity
+- `k          :: Float64`               - thermal conductivity
+- `κ          :: Float64`               - thermal diffusivity
+- `T0         :: Union{Int64, Float64}` - phase transition reference T
+- `P0         :: Union{Int64, Float64}` - phase transition reference P
+- `a          :: Float64`               - Simon pressure
+- `b          :: Float64`               - Simon gradient
+- `B          :: Float64`               - effective temperature exponent
+- `∇          :: Float64`               - adiabtatic gradient
+- `T_eq       :: Union{Int64, Float64}` - radiative equilibrium temperature
+- `T_ef       :: Union{Int64, Float64}` - Today's effective temperature
+- `T1         :: Union{Int64, Float64}` - Temp at 1 bar
+- `η0         :: Float64`               - reference viscosity
+- `A          :: Int64`                 - viscosity exponent
+- `Ra         :: Int64`                 - critical Rayleigh number
+- `μ_f        :: NTuple{3, Union{Int64, Float64}}`
+                               - rhealogoy parameters
+                                  (1 - SLS(mu factor),
+                                   2,3 - Andrade(alpha, beta))
+- `rhea_model :: Int64`        - modulus model(1:maxwell, 2:SLS, 3:Andrade)
+"""
+struct Planet
+
+    name :: String
+
+    # structure parameters
+    layers :: Int64
+    R      :: Float64
+    M      :: Float64
+    GM     :: Float64
+
+    # orbital parameters
+    ω :: Float64
+
+    # thermal parameters
+    C_p  :: Union{Int64, Float64}
+    α    :: Float64
+    k    :: Float64
+    κ    :: Float64
+    T0   :: Union{Int64, Float64}
+    P0   :: Union{Int64, Float64}
+    a    :: Float64
+    b    :: Float64
+    B    :: Float64
+    ∇    :: Float64
+    T_eq :: Union{Int64, Float64}
+    T_ef :: Union{Int64, Float64}
+    T1   :: Union{Int64, Float64}
+
+    # rheaology parameters
+    η0         :: Float64
+    A          :: Int64
+    Ra         :: Int64
+    μ_f        :: NTuple{3, Union{Int64, Float64}}
+    rhea_model :: Int64
+
+end
+
+"""
+    Moon
+
+Moon struct used for PlanetEvolution.jl. Primarily for the tidal calculation and
+orbital migration.
+
+# Fields
+- `name  :: String`  - name of moon
+- `m     :: Float64` - mass of moon
+- `gm    :: Float64` - gravitational mass of moon
+- `a     :: Float64` - orbital radius
+- `i     :: Float64` - inclination
+- `retro :: Bool`    - is moon in retrograde motion
+"""
+struct Moon
+
+    name :: String
+
+    # structure parameters
+    m  :: Float64
+    gm :: Float64
+
+    # orbital parameters
+    a :: Float64
+    i :: Float64
+
+    # flags
+    retro :: Bool
+
+end
+
+"""
+    lumin_internal(R::Float64, T_ef::Union{Int64, Float64},
+                   T_eq::Union{Int64, Float64})::Float64
+
+Calculates the total luminosity of the interior.
+
+# Arguments
+- `R::Float64`    - radius
+- `T_ef::Union{Int64, Float64}` - effective temperature
+- `plnt::Planet` - Planet struct for radiative equilibrium temperature
+"""
+function lumin_internal(R::Float64, T_ef::Union{Int64, Float64},
+                        plnt::Planet)::Float64
+    4π*R^2 * σ.val * (T_ef^4 - plnt.T_eq^4)
+end
+
+"""
+    lumin_core(T1::Union{Int64, Float64}, Ti::Float64, c::Float64,
+               P_c::Float64, T_c::Float64, ρ_c::Float64, g_c::Float64,
+               P1::Float64,
+               plnt::Planet)::NamedTuple{(:L_c, :K, :ΔT, :Γ, :Ra), Float64}
+
+Calculates the luminosty due to the core. Refer to Stixrude et al. 2021 (eq 9-16).
+
+# Arguments
+- `T1::Union{Int64, Float64}`  - temperature of envelope
+- `Ti::Float64`  - temperature at top of thermal boundary
+- `c::Float64`   - radius of core
+- `P_c::Float64` - pressure at top of the core
+- `T_c::Float64` - temperature at top of the core
+- `ρ_c::Float64` - density at top of the core
+- `g_c::Float64` - gravity at top of the core
+- `P1::Float64`  - reference pressure
+- `plnt::Planet` - planet struct
+"""
+function lumin_core(T1::Union{Int64, Float64}, Ti::Float64, c::Float64,
+                    P_c::Float64, T_c::Float64, ρ_c::Float64, g_c::Float64,
+                    P1::Float64,
+                    plnt::Planet)::NamedTuple{(:L_c, :K, :ΔT, :Γ, :Ra), Float64}
+    if(Ti < 0)
+        P_c = plnt.P0 - plnt.a + 1e-6
+        Ti = temp_adiabat(P_c, T1, P1, plnt.∇)
+    end
+
+    P_ratio = (P_c / P1)^plnt.∇
+
+    ΔT = Ti - T_c
+    ΔP = P_c - plnt.P0
+
+    Γ = cc_slope(P_c, plnt.P0, plnt.T0, plnt.a, plnt.b)
+    Γ_a = temp_distribution(P_c, T_c, plnt.∇)
+    ΔΓ = Γ - Γ_a
+
+    h = visc_height(T_c, ρ_c, g_c, plnt.A, Γ, Γ_a)
+    if(h > c) h = c end
+
+    T_m = temp_melting(P_c, plnt.P0, plnt.T0, plnt.a, plnt.b)
+
+    η = planet_eta(plnt.η0, plnt.A, T_m, T_c)
+
+    Ra = ρ_c * plnt.α * abs(ΔT) * g_c * abs(h)^3 / (plnt.κ * η)
+
+    K = ρ_c * plnt.α * g_c / (plnt.Ra * η * plnt.κ)
+    L_c = plnt.k * K^(1/3) * ((h / c)^(4))^(1/3) * (ΔT^4)^(1/3) * 4*π*c^2
+
+    # this is value next to ∂c/∂t term in the derivation
+    K = - 1 / (ρ_c * g_c * ΔΓ) * P_ratio
+
+    return (L_c=L_c, K=K, ΔT=ΔT, Γ=Γ_a/ΔΓ, Ra=Ra)
+end
+
+end # module
---
+++ src/thermal.jl
@@ -0,0 +1,172 @@
+export planet_eta
+export temp_effective, temp_adiabat, temp_melting
+export cc_slope, temp_distribution, visc_height, thermal_diff
+export layer_density, thermal_inertia
+
+"""
+    planet_eta(η0::Float64, A::Float64, T_m::Float64, T::Float64)::Float64
+
+Calculates viscosity. Refer to Stixrude et al. 2021 (eq 15).
+
+# Arguments
+- `η0::Float64`  - reference viscosity
+- `A::Float64`   - viscosity exponent
+- `T_m::Float64` - melting temperature
+- `T::Float64`   - temperature
+"""
+function planet_eta(η0::Float64, A::Float64, T_m::Float64, T::Float64)::Float64
+    η0 * exp(A * (T_m/T - 1))
+end
+
+"""
+    temp_effective(T1::Union{Int64, Float64}, B::Float64)::Float64
+
+Calcultes the effective temperature where it is the temperature the planet would
+have in absence of solar luminosity. Refer to Stixrude et al. 2021 (eq 13) or
+Guillot et al. 1995.
+
+# Arguments
+- `T1::Union{Int64, Float64}` - temperature
+- `B::Float64`  - constant of normalization
+"""
+function temp_effective(T1::Union{Int64, Float64}, B::Float64)::Float64
+    (T1 / B)^(1/1.244)
+end
+
+"""
+    temp_adiabat(P::Float64, Tx::Float64, Px::Float64, ∇::Float64)::Float64
+
+Calculates the adiabatic temperature. Refer to Stixrude et al. 2021 (eq 12).
+
+# Arguments
+- `P::Float64`  - pressure
+- `Tx::Float64` - temperature
+- `Px::Float64` - reference pressure
+- `∇::Floa64`  - adiabatic gradient
+"""
+function temp_adiabat(P::Float64, Tx::Float64, Px::Float64, ∇::Float64)::Float64
+    Tx * (P / Px)^∇
+end
+
+"""
+    temp_melting(P::Float64, P0::Union{Int64, Float64},
+                 T0::Union{Int64, Float64}, a::Float64, b::Float64)::Float64
+
+Calculates the melting temperature. Refer to Stixrude et al. 2021 (eq 11).
+
+# Arguments
+- `P::Float64`   - pressure
+- `P0::Union{Int64, Float64}` - phase transition reference pressure
+- `T0::Union{Int64, Float64}` - phase transition reference temperature
+- `a::Float64`   - Simon pressure
+- `b::Float64`   - Simon exponent
+"""
+function temp_melting(P::Float64, P0::Union{Int64, Float64},
+                      T0::Union{Int64, Float64}, a::Float64, b::Float64)::Float64
+    T0 * (1 + (P - P0) / a)^b
+end
+
+"""
+    cc_slope(P::Float64, P0::Union{Int64, Float64},
+             T0::Union{Int64, Float64}, a::Float64, b::Float64)::Float64
+
+Calculates the Clausius-Clapeyron slope of the phase boundary, Γ, where
+Γ = d(T_m)/dP.
+
+# Arguments
+- `P::Float64`   - pressure
+- `P0::Union{Int64, Float64}`  - phase transition reference pressure
+- `T0::Union{Int64, Float64}`  - phase transition reference temperature
+- `a::Float64`   - Simon pressure
+- `b::Float64`   - Simon exponent
+"""
+function cc_slope(P::Float64, P0::Union{Int64, Float64},
+                  T0::Union{Int64, Float64}, a::Float64, b::Float64)::Float64
+    T0 * b / a * (1 + (P - P0) / a)^(b - 1)
+end
+
+"""
+    temp_distribution(P_c::Float64, T_c::Float64, ∇::Float64)::Float64
+
+Calculates the slope, Γ_a, of the planetary temperature distribution at `T_c` and
+`P_c`.
+
+# Arguments
+- `P_c::Float64` - pressure at top of the core
+- `T_c::Float64` - temperature at top of the core
+- `∇::Float64`   - adiabatic gradient
+"""
+function temp_distribution(P_c::Float64, T_c::Float64, ∇::Float64)::Float64
+    (T_c / P_c) * ∇
+end
+
+"""
+    visc_height(T_c::Float64, ρ_c::Float64, g_c::Float64, A::Int64,
+                Γ::Float64, Γ_a::Float64)::Float64
+
+Calculates the viscous scale height, h.
+
+# Arguments
+- `T_c::Float64` - temperature at top of the core
+- `ρ_c::Float64` - density at top of the core
+- `g_c::Float64` - gravity at top of the core
+- `A::Int64`     - viscosity exponent
+- `Γ::Float64`   - Clausius-Clapeyron slope of phase boundary
+- `Γ_a::Float64` - slope of planetary temp distribution
+"""
+function visc_height(T_c::Float64, ρ_c::Float64, g_c::Float64, A::Int64,
+                     Γ::Float64, Γ_a::Float64)::Float64
+    T_c / (ρ_c * g_c * A * (Γ - Γ_a))
+end
+
+"""
+    thermal_diff(k::Float64, ρ::Float64, C_p::Union{Int64, Float64})::Float64
+
+Calculates the thermal diffusivity.
+
+# Arguments
+- `k::Float64`   - thermal conductivity
+- `ρ::Float64`   - density
+- `C_p::Union{Int64, Float64}` - specific heat
+"""
+function thermal_diff(k::Float64, ρ::Float64, C_p::Union{Int64, Float64})::Float64
+    k / (ρ * C_p)
+end
+
+"""
+    layer_density(r::Float64, P::Float64, Px::Float64, ∇::Float64,
+                  ρ::Float64)::Float64
+
+Function integrated for luminosity. Refer to Stixrude et al. 2021 (eq 8-9) or
+Fortney & Nettelmann 2010.
+
+# Arguments
+- `r::Float64`  - radius
+- `P::Float64`  - pressure
+- `Px::Float64` - reference pressure
+- `∇::Float64`  - adiabatic gradient
+- `ρ::Float64`  - density
+"""
+function layer_density(r::Float64, P::Float64, Px::Float64, ∇::Float64,
+                       ρ::Float64)::Float64
+    (P/Px)^∇ * ρ * r^2
+end
+
+"""
+    thermal_inertia(p, P::Real, a::Real, b::Real)
+
+Integrates the `layer_density` for the luminosity of a planet.
+
+# Arguments
+- `p`        - tuple holding several parameters
+- `P::Real`  - pressure
+- `r0::Real` - radial start
+- `r1::Real` - radial end
+"""
+function thermal_inertia(p, P::Real, r0::Real, r1::Real)
+
+    A, err = quadgk(r -> layer_density(r, p.i(p.P[:,1], p.P[:,2], r)[1], P,
+                                       p.plnt.∇, p.ρ(r)), r0, r1)
+
+    return A
+end
